a2d2ef9a38110bf897041cb7562454a3a024d32b
KotonBads <kotonbads@duck.com>
2022-06-20 16:59:20 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import click
+import contextlib
+import requests
+import json
+
+
+# ANSI Color Codes
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+PURPLE = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+RESET = "\033[0m"
+BOLD = "\033[1m"
+
+
+def fetch(word: str) -> dict:
+    return requests.get(
+        f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+    ).json()
+
+
+def write(word: str):
+    with open("word.json", "w") as f:
+        json.dump(fetch(word), f)
+
+
+def phoentic(res: dict) -> None:
+    phonetic = res[0]["phonetics"]
+
+    with contextlib.suppress(KeyError, UnboundLocalError):
+        for i in phonetic:
+
+            if i["text"] and i["audio"]:
+                text = i["text"]
+                audio = i["audio"]
+                accent = audio.split("/")[-1]
+
+        print(f"\033]8;;{audio}\033\\{CYAN}{text} ({accent})\033]8;;\033\\{RESET}")
+
+
+def definition(res: dict) -> None:
+    definition = res[0]["meanings"][0]["definitions"]
+    for i in definition:
+        print(f"• {GREEN}{i['definition']}{RESET}")
+
+
+@click.command()
+@click.argument("word")
+def main(word: str) -> None:
+    """
+    A CLI tool for fetching the definition of a word.
+    """
+
+    RES = fetch(word)
+
+    print(f"{BOLD}{WHITE}[{word}]{RESET}")
+    phoentic(RES)
+    print(f"\n{BOLD}{YELLOW}Definitions:{RESET}")
+    definition(RES)
+
+
+if __name__ == "__main__":
+    main()
